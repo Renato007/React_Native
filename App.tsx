4f8624ce4bf29862dd84a39f77a9f2f91918678b
Renato Soares da Silva <renatosilsoa@gmail.com>
2021-04-21 18:58:45 -0300

    finalização da primeira tela

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+//forma de importar uma pagina
 import { Welcome } from './src/pages/Welcome';
+// @expo-google-fonts é a melhor forma para carregar fontes 
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+// permite 
+import AppLoading from 'expo-app-loading';
 
-export default function App(){
-  return(
-  <Welcome/>
-
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+// enquanto as fontes não estiverem carregadas segurar na tela de espera
+  if (!fontsLoaded)
+    return <AppLoading/>
+    
+ return(
+  <Welcome />
   )
 }
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,38 +5,52 @@ import {
     Image,
     TouchableOpacity,
     StyleSheet,
-    Dimensions
-} from 'react-native';
+    Dimensions,
+    View
+}
+    from 'react-native';
+import { Feather } from '@expo/vector-icons'
+
 import wateringImg from '../assets/watering.png';
+
+//boas praticas colocar uma pasta com as paletas de cores
 import colors from '../styles/colors';
+
+//boas praticas colocar uma pasta com as fontes
+import fonts from '../styles/fonts';
+
+
 export function Welcome() {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie{'\n'} suas plantas{'\n'} de forma fácil
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie{'\n'} suas plantas de {'\n'} forma fácil
             </Text>
 
-            <Image
-                source={wateringImg}
-                style={styles.imagen}
-                resizeMode="contain" // permite não fixar a imagem
-             
-            />
+                <Image
+                    source={wateringImg}
+                    style={styles.imagen}
+                    resizeMode="contain" // permite não fixar a imagem
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. nós cuidamos de lembrar você sempre que precisar.
+                />
+
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. nós cuidamos de lembrar você sempre que precisar.
             </Text>
 
-            <TouchableOpacity
+                <TouchableOpacity
 
-                style={styles.button}
-                activeOpacity={0.7}
-            >
-                <Text style={styles.buttontex}>
-                    >
-                </Text>
-            </TouchableOpacity>
+                    style={styles.button}
+                    activeOpacity={0.7}
+                >
+                    <Feather
+                        name="chevron-right"
+                        style={styles.buttonIcon}
+                    />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -46,19 +60,29 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: "space-around" // between usar o around para que ele não cole nas bordas. 
     },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "space-around",
+        //isso tudo para colocar o padding
+        paddingHorizontal: 20
+    },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
 
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
     },
     button: {
         backgroundColor: colors.green,
@@ -76,5 +100,9 @@ const styles = StyleSheet.create({
     imagen: {
         // evite trabalhar com valores fixos importe no react-native o pacote dimension
         height: Dimensions.get('window').width * 0.7
+    },
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white
     }
 })
